9d63be64c5ea3b27849bd011a253b8f144344ab3
mazefeng <vincent.ma.zefeng@gmail.com>
2014-10-01 12:08:57 +0800

    update common

--- common.py
+++ common.py
@@ -2,7 +2,42 @@
 
 import sys
 
+'''
+    All regression/classification data are stored in the same format as LIBSVM.
+
+    Use read_sparse_data for loading sparse data
+    Use read_dense_data for loading dense data
+
+    When loading dense data, feature id must be integer.
+'''
+
 def read_data(fp_data):
+    return read_sparse_data(fp_data)
+
+def read_dense_data(fp_data):
+    X, Y = list(), list()
+    max_len = 0
+
+    for line in fp_data:
+        line_arr = line.strip().split()
+        Y.append(line_arr[0])
+        x = list()
+        for kv in line_arr[1 : ]:
+            k, v = kv.split(':')
+            k = int(k) - 1
+            v = float(v)
+            x.extend([0.0] * (k - len(x) + 1))
+            if len(x) > max_len:
+                max_len = len(x)
+            x[k] = v
+        X.append(x)
+
+    for x in X:
+        x.extend([0.0] * (max_len - len(x))) 
+
+    return X, Y
+
+def read_sparse_data(fp_data):
     X, Y = list(), list()
 
     for line in fp_data:
@@ -10,10 +45,77 @@ def read_data(fp_data):
         Y.append(line_arr[0])
         x = list()
         for kv in line_arr[1 : ]:
-            w, c = kv.split(':')
-            x.append([w, float(c)])
+            k, v = kv.split(':')
+            x.append([k, float(v)])
         X.append(x)
 
     return X, Y
 
+def read_sequence_data(fp_data):
+    X, S = list(), list()
+
+    L = list()
+    I = list()
+
+    for c, line in enumerate(fp_data):
+        x, s = line.strip().split('/')
+        L.append([x, s])
+        if x == '###':
+            I.append(c)
+
+    for t in range(len(I) - 1):
+        x_list = [x for x, s in L[I[t] + 1 : I[t + 1]]]
+        s_list = [s for x, s in L[I[t] + 1 : I[t + 1]]]
+        X.append(x_list)
+        S.append(s_list)
+
+    return X, S
+
+def plot_sequence_data(x, s, line_width = 128):
+
+    x_out = list()
+    t_out = list()
+    s_out = list()
+
+    for p, q in zip(x, s):
+
+        l = max(len(p), len(q))
+        
+        d = l - len(q)
+        m = d / 2
+        n = d - m
+        x_out.append(p + ' ' * (l - len(p)))
+        s_out.append('-' * m + q + '-' * n)
+
+        m = (l - 1) / 2
+        n = l - 1 - m
+        t_out.append(' ' * m + '|' + ' ' * n)
+    
+    s_line = '-'.join(s_out)
+    t_line = ' '.join(t_out)
+    x_line = ' '.join(x_out)
+
+    if len(s_line) > line_width:
+        for i in range(len(s_line) / line_width + 1):
+            print >> sys.stderr, s_line[i * line_width : (i + 1) * line_width]
+            print >> sys.stderr, t_line[i * line_width : (i + 1) * line_width]
+            print >> sys.stderr, x_line[i * line_width : (i + 1) * line_width]
+    else:
+        print >> sys.stderr, s_line
+        print >> sys.stderr, t_line
+        print >> sys.stderr, x_line 
+
+
+def map_label(Y):
+    m = {k:v for v,k in enumerate(set(Y))}
+    Y_map = [0] * len(Y)
+    for i, k in enumerate(Y):
+        Y_map[i] = m[k]
+    return Y_map
+
+
+if __name__ == '__main__':
+
+    X, S = read_sequence_data(open('data/pos-tagging/entrain'))
+    plot_sequence_data(X[1], S[1])
 
