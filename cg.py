8ea3b7aa330510b475043e90db165c1072385541
mazefeng <vincent.ma.zefeng@gmail.com>
2014-10-10 16:01:21 +0800

    update Conjugate Gradient routine

--- cg.py
+++ cg.py
@@ -5,9 +5,13 @@ from math import isinf
 from math import sqrt
 import numpy as np
 
-from common import print_info
+from common import trace
 
-def CG(f, w, **argc):
+def CG(f, w, max_fc = 95, **argc):
+    '''
+    conjugate gradient routine for optimization
+    '''
+
     INT = 0.1
     EXT = 3.0
     MAX = 20
@@ -16,61 +20,62 @@ def CG(f, w, **argc):
     RHO = SIG / 2
    
     red = 1.0
-    max_ls = 200
-    max_func_eval = 200
    
     # if length > 0: S = 'Linesearch'
     # else: S = 'Function evaluation'
     
-    i = j = 0
-    ls_failed = False
+    I = J = 1
+    LS_failed = False
     f0, df0 = f(w, **argc)
-    fw = [f0]
-    j = j + 1
+    print >> sys.stderr, 'Iter = %4.4i    Cost = %lf' % (I, f0)
 
     s = -df0
     d0 = float(-s.T * s)
     w3 = red / (1.0 - d0)
     
-    while i < max_ls:
-        i = i + 1
+    while I < max_fc:
         w0, F0, dF0 = w, f0, df0
-        M = MAX
-        # M = min(MAX, -length - i)
+        M = min(MAX, max_fc - I)
         while True:
             w2, f2, d2, f3, df3 = 0, f0, d0, f0, df0
             success = False
             while not success and M > 0:
                 try:
-                    M = M - 1
-                    j = j + 1
+                    M -= 1
+                    I += 1
                     f3, df3 = f(w + w3 * s, **argc)
-                    if isnan(f3) or isinf(f3) or np.any(np.isnan(df3) + np.isinf(df3)): raise NameError, ('error')
+                    print >> sys.stderr, 'Iter = %4.4i    Cost = %lf' % (I, f3)
+                    if isnan(f3) or isinf(f3) or np.any(np.isnan(df3) + np.isinf(df3)):
+                        raise NameError, ('error')
                     success = True
                 except Exception, e:
                     print >> sys.stderr, 'Exception = %s'  % e
-                    print_info()
+                    trace()
                     w3 = (w2 + w3) / 2.0
             if f3 < F0:
                 w0, F0, dF0 = w + w3 * s, f3, df3
             d3 = float(df3.T * s)
-            if d3 > SIG * d0 or f3 > f0 + w3 * RHO * d0 or M == 0: break
+            if d3 > SIG * d0 or f3 > f0 + w3 * RHO * d0 or M == 0:
+                break
             w1, f1, d1 = w2, f2, d2
             w2, f2, d2 = w3, f3, d3
             A = 6 * (f1 - f2) + 3 * (d2 + d1) * (w2 - w1)
             B = 3 * (f2 - f1) - (2 * d1 + d2) * (w2 - w1)
-            T = B * B - A * d1 * (w2 - w1)
             # if not isinstance(w3, float) or isnan(w3) or isinf(w3) or w3 < 0: w3 = w2 * EXT
             try:
                 w3 = w1 - d1 * (w2 - w1) ** 2 / (B + sqrt(B * B - A * d1 * (w2 - w1)))
+                # add sth
             except Exception, e:
                 print >> sys.stderr, 'Exception = %s' % e
-                print_info()
+                trace()
                 w3 = w2 * EXT
                 continue
-            if w3 < 0: w3 = w2 * EXT
-            elif w3 > w2 * EXT: w3 = w2 * EXT
-            elif w3 < w2 + INT * (w2 - w1): w3 = w2 + INT * (w2 - w1)
+            if w3 < 0:
+                w3 = w2 * EXT
+            elif w3 > w2 * EXT:
+                w3 = w2 * EXT
+            elif w3 < w2 + INT * (w2 - w1):
+                w3 = w2 + INT * (w2 - w1)
       
         while (abs(d3) > -SIG * d0 or f3 > f0 + w3 * RHO * d0) and M > 0:
             if d3 > 0 or f3 > f0 + w3 * RHO * d0:
@@ -86,37 +91,39 @@ def CG(f, w, **argc):
                     w3 = w2 + (sqrt(B * B - A * d2 * (w4 - w2) ** 2) - B) / A
             except Exception, e:
                     print >> sys.stderr, 'Exception = %s' % e
-                    print_info()
+                    trace()
                     w3 = float('NaN')
-            if isnan(w3) or isinf(w3): w3 = (w2 + w4) / 2
+            if isnan(w3) or isinf(w3):
+                w3 = (w2 + w4) / 2
             w3 = max(min(w3, w4 - INT * (w4 - w2)), w2 + INT * (w4 - w2))
             f3, df3 = f(w + w3 * s, **argc)
             if f3 < F0:
                 w0, F0, dF0 = w + w3 * s, f3, df3
-            M = M - 1
-            j = j + 1
+            M -= 1
+            I += 1
+            print >> sys.stderr, 'Iter = %4.4i    Cost = %lf' % (I, f3)
             d3 = float(df3.T * s)
       
         if abs(d3) < -SIG * d0 and f3 < f0 + w3 * RHO * d0:
             w, f0 = w + w3 * s, f3
-            fw.append(f0)
-            print >> sys.stderr, 'Line_search = %4.4i    Cost = %lf' % (i, f0)
+            print >> sys.stderr, 'Line_search = %4.4i    Cost = %lf' % (J, f0)
+            J += 1
             s = float((df3.T * df3 - df0.T * df3) / (df0.T * df0)) * s - df3
             df0 = df3
             d3, d0 = d0, float(df0.T * s)
             if d0 > 0:
                 s, d0 = -df0, float(-s.T * s)
             w3 = w3 * min(RATIO, d3 / (d0 - sys.float_info.min))
-            ls_failed = False
+            LS_failed = False
         else:
             w, f0, df0 = w0, F0, dF0
-            if ls_failed or i > max_ls: break
+            if LS_failed or I > max_fc:
+                break
             s, d0 = -df0, float(-s.T * s)
             w3 = 1.0 / (1.0 - d0)
-            ls_failed = True
+            LS_failed = True
     print >> sys.stderr, ''
     return w
-
 
 def f(x):
     return float(x * x - 2 * x + 1), 2 * x - 2 
